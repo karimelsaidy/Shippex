cee8e0b6613e69af3e2058ec717801ac2f4d9d58
karimelsaidy <karimabdlelkarim@gmail.com>
2024-04-16 05:23:45 +0200

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,36 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'], // removing consoles.log from app during release (production) versions
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@/src': './src',
+          '@/assets': './src/assets',
+          '@/images': './src/assets/images',
+          '@/components': './src/components',
+          '@/hooks': './src/hooks',
+          '@/config': './src/config',
+          '@/navigation': './src/navigation',
+          '@/screens': './src/screens',
+          '@/services': './src/services',
+          '@/storage': './src/storage',
+          '@/store': './src/store',
+          '@/utils': './src/utils',
+          '@/tw': './src/utils/tw',
+          '@/i18n': './src/i18n',
+          '@/tokens': './tokens',
+          '@/validation': './src/utils/validation',
+        },
+      },
+    ],
     [
       'module:react-native-dotenv',
       {
@@ -12,4 +43,6 @@ module.exports = {
         verbose: false,
       },
     ],
+    'react-native-reanimated/plugin',
+  ],
 };
