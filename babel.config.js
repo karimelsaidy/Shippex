3431592e33e07067adc1eb0a343aab995745275a
karimelsaidy <karimabdlelkarim@gmail.com>
2024-04-16 05:13:35 +0200

    implement env files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@/env',
+        path: 'env/.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
 };
