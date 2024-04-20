0484c3ac19e5553962d7d7a381545228c7ca8110
karimLeanNode <karim@leannode.com>
2024-04-20 12:28:53 +0200

    implement login service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,17 +1,33 @@
-import {API_BASE} from '@/env';
-import {appLang} from '@/utils';
+import {userData} from './dummyData';
 
 export type logInServiceType = {
-  email: string;
+  userNameOrEmail: string;
   password: string;
 };
 
-export async function logInService(params: logInServiceType): Promise<any> {
-  return (
-    `${API_BASE}auth/individual/phone/register`,
-    {
-      method: 'post',
-      data: params,
-    }
-  );
+export async function logInService({
+  userNameOrEmail,
+  password,
+}: logInServiceType): Promise<any> {
+  const isValidUser = userNameOrEmail.length > 3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        status: isValidUser,
+        ...(isValidUser
+          ? {
+              user: userData,
+            }
+          : {
+              feedback: {
+                message: {
+                  en: 'Incorrect username or password',
+                  ar: 'اسم المستخدم أو كلمة المرور غير صحيحة',
+                },
+              },
+            }),
+      });
+    }, 3000);
+  });
 }
